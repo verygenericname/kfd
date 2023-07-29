0848e9dd9a350ba55caad91282727b115fae6c30
wh1te4ever <88495487+wh1te4ever@users.noreply.github.com>
2023-07-29 21:57:01 +0900

    Add description to vnode.h

--- kfd/fun/vnode.h
+++ kfd/fun/vnode.h
@@ -13,8 +13,33 @@
 uint64_t getVnodeAtPath(char* filename);
 uint64_t findRootVnode(void);
 
+/*
+Description:
+  Hide and reveal file or directory.
+*/
 uint64_t funVnodeHide(char* filename);
+
+/*
+Description:
+  Perform chown to file or directory.
+*/
 uint64_t funVnodeChown(char* filename, uid_t uid, gid_t gid);
+
+/*
+Description:
+  Perform chmod to file or directory.
+*/
 uint64_t funVnodeChmod(char* filename, mode_t mode);
+
+/*
+Description:
+  Redirect directory to another directory.
+  Only work when mount points of directories are same.
+*/
 uint64_t funVnodeRedirectFolder(char* to, char* from);
+
+/*
+Description:
+  Perform overwrite file data to file.
+*/
 uint64_t funVnodeOverwriteFile(char* to, char* from);
